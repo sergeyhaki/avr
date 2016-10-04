d70a2f545dfe32caecfdf9ca29f07a3f517ccb7a
sergeyhaki <navent@mail.ru>
2016-10-04 18:41:46 +0300

    20161004

--- git push.bat
+++ git push.bat
@@ -4,7 +4,7 @@ call git.exe remote add origin https://github.com/sergeyhaki/avr.git
 
 call git.exe add . 
 
-call git.exe commit -m "from bat"
+call git.exe commit -m "%Date:~6,4%%Date:~3,2%%Date:~0,2%"
 
 call git.exe push -u origin master
 
